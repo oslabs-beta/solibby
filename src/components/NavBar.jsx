9e82d94621d492d24f504d128bec19fc2cc26358
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-05-03 21:11:19 -0400

    Refactor NavBar component to add side menu functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 // solidJS
 import { useContext, createEffect, createSignal } from 'solid-js';
 import { ComponentContext } from '../context/ComponentContext';
+import { DrawerIsOpenContext } from '../context/DrawerIsOpenContextProvider';
 
 // assets
 import solibeeLogoLight from '../../assets/solibee-logo2.png';
@@ -9,11 +10,21 @@ import gitLogo from '../../assets/github-mark.png';
 import gitLogoWhite from '../../assets/github-mark-white.png';
 import sunIcon from '../../assets/sun-svgrepo-com.svg';
 import nightIcon from '../../assets/night-svgrepo-com.svg';
+import expandSideMenu from '../../assets/expand-menu.svg';
+import expandSideMenuWhite from '../../assets/expand-menu-white.svg';
+
+
 
 export default function NavBar() {
   const { components } = useContext(ComponentContext);
+  const { drawerIsOpen, setDrawerIsOpen } = useContext(DrawerIsOpenContext);
   const href = `/component/${components[0].toLowerCase().replaceAll(' ', '')}`;
   const [isDarkMode, setIsDarkMode] = createSignal(false);
+ 
+  //it will set the side menu to open or close
+  function handleSideMenu(){
+    return setDrawerIsOpen({open: !drawerIsOpen.open}); // Toggle side menu
+  }
 
   function handleClick() {
     setIsDarkMode(!isDarkMode()); // Toggle dark mode
@@ -62,23 +73,22 @@ export default function NavBar() {
 
   // modeIcon.removeEventListener('click', handleClick);
   // modeIcon.addEventListener('click', handleClick);
-
   return (
-    <header class='bg-white-200 sticky top-0 z-50 flex h-16 w-screen justify-center border-b border-orange-100 px-5 py-2 text-sm font-medium text-slate-700 backdrop-blur-sm dark:border-orange-300'>
-      <div class='flex w-full max-w-6xl items-center justify-between'>
+    <header class=' bg-white-200 sticky top-0 z-40 flex h-16 w-screen justify-center border-b border-orange-100 px-5 py-2 text-sm font-medium text-slate-700  backdrop-blur-3xl dark:border-orange-300'>
+      <div class=' flex w-full max-w-6xl items-center justify-between'>
         <div class='flex items-center text-font'>
           <a
             href='/'
-            class='mr-10 flex items-center justify-center font-pacifico text-2xl'
+            class='mr-4 flex items-center justify-center font-pacifico text-2xl md:mr-10'
           >
             <img
-              class='mr-2 h-8'
+              class='mr-2 h-6 md:h-8'
               src={isDarkMode() ? solibeeLogoDark : solibeeLogoLight}
               alt='the solibee logo: a bee in a honeycomb'
             />
             Solibee
           </a>
-          <span class='mr-6 mt-1 hover:text-orange-200'>
+          <span class='mr-2 mt-1 hover:text-orange-200 md:mr-6'>
             <a href='/introduction'>Docs</a>
           </span>
           <span class='mt-1 hover:text-orange-200'>
@@ -91,22 +101,34 @@ export default function NavBar() {
             target='_blank'
             href='https://github.com/oslabs-beta/solibee/'
           >
-            <span class=''>
-              <img
-                id='github-icon'
-                class='h-6'
-                src={isDarkMode() ? gitLogo : gitLogoWhite}
-                alt='GitHub logo'
-              />
-            </span>
+            <img
+              id='github-icon'
+              class='h-6'
+              src={isDarkMode() ? gitLogo : gitLogoWhite}
+              alt='GitHub logo'
+            />
           </a>
           <img
             id='mode-icon'
-            class='ml-6 h-6 '
+            class='ml-2 h-6 md:ml-6 '
             src={isDarkMode() ? nightIcon : sunIcon}
             alt='Sun icon'
             onClick={handleClick}
           />
+      
+          <button
+            onClick={handleSideMenu}
+            aria-haspopup='dialog'
+            aria-expanded={drawerIsOpen.open}
+            aria-controls=''
+          >
+            <span class='sr-only'>Open navigation to the left side</span>
+            <img
+              class='ml-2 h-6 md:hidden'
+              src={isDarkMode() ? expandSideMenuWhite : expandSideMenu}
+              alt='expand menu icon'
+            />
+          </button>
         </div>
       </div>
     </header>
