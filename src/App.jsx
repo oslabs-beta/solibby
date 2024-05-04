df125347f3566d7576e0761cf01453197c67430a
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-05-03 20:41:55 -0400

    Update App.jsx to include DrawerMenu component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { createSignal, createEffect, onCleanup } from 'solid-js';
 import NavBar from './components/NavBar';
 import MainContainer from './containers/MainContainer';
 import Footer from './components/Footer';
+import DrawerMenu from './components/DrawerMenu';
+
 
 function App() {
   onCleanup(() => {
     document.documentElement.remove('data-theme');
   });
+  
 
   return (
-    <div class='flex min-h-screen flex-col overflow-x-hidden bg-light bg-[length:5rem] text-font dark:bg-dark'>
-      <div class='flex grow flex-col'>
+    <div
+      id='app'
+      class=' flex min-h-screen flex-col overflow-x-hidden bg-light bg-[length:5rem] text-font dark:bg-dark'
+    >
+      <div class=' flex grow flex-col'>
+        <DrawerMenu /> 
         <NavBar />
         <MainContainer />
       </div>
